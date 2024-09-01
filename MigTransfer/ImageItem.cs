8fc4fec4273bab989228c51acedf32d2b9a4fbe4
José Armiche Garcia Ramos <armichegarcia@gmail.com>
2024-09-01 15:14:06 +0100

    Añadido Casilla de selección a cada Item

--- MigTransfer/ImageItem.cs
+++ MigTransfer/ImageItem.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace MigTransfer
@@ -9,22 +8,19 @@ namespace MigTransfer
     {
         private PictureBox pictureBox;
         private CheckBox checkBox;
-        private Image originalImage;
 
         public ImageItem(Image image)
         {
-            originalImage = image ?? throw new ArgumentNullException(nameof(image));
-            InitializeComponents();
+            InitializeComponents(image);
         }
 
-        private void InitializeComponents()
+        private void InitializeComponents(Image image)
         {
             this.Size = new Size(256, 414); // Establecer el tamaño del UserControl
-            this.BorderStyle = BorderStyle.FixedSingle; // Agregar un borde para visualizar mejor
 
             pictureBox = new PictureBox
             {
-                Image = originalImage,
+                Image = image,
                 SizeMode = PictureBoxSizeMode.StretchImage,
                 Dock = DockStyle.Fill, // Ajustar el PictureBox al tamaño del UserControl
                 Margin = new Padding(10)
@@ -33,54 +29,15 @@ namespace MigTransfer
             checkBox = new CheckBox
             {
                 Location = new Point(5, 5),
-                Visible = false
+                BackColor = Color.Transparent, // Asegurar que el fondo sea transparente
+                AutoSize = true // Asegurar que la casilla se ajuste a su contenido
             };
 
             this.Controls.Add(pictureBox);
             this.Controls.Add(checkBox);
 
-            pictureBox.MouseEnter += PictureBox_MouseEnter;
-            pictureBox.MouseLeave += PictureBox_MouseLeave;
-            checkBox.CheckedChanged += CheckBox_CheckedChanged;
-        }
-
-        private void PictureBox_MouseEnter(object? sender, EventArgs e)
-        {
-            checkBox.Visible = true;
-        }
-
-        private void PictureBox_MouseLeave(object? sender, EventArgs e)
-        {
-            if (!checkBox.Checked)
-            {
-                checkBox.Visible = false;
-            }
-        }
-
-        private void CheckBox_CheckedChanged(object? sender, EventArgs e)
-        {
-            if (checkBox.Checked)
-            {
-                pictureBox.Image = ChangeImageOpacity(originalImage, 0.5f);
-            }
-            else
-            {
-                pictureBox.Image = ChangeImageOpacity(originalImage, 1.0f);
-            }
-        }
-
-        private Image ChangeImageOpacity(Image image, float opacity)
-        {
-            Bitmap bmp = new Bitmap(image.Width, image.Height);
-            using (Graphics gfx = Graphics.FromImage(bmp))
-            {
-                ColorMatrix matrix = new ColorMatrix();
-                matrix.Matrix33 = opacity;
-                ImageAttributes attributes = new ImageAttributes();
-                attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-                gfx.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
-            }
-            return bmp;
+            // Asegurarse de que el CheckBox esté al frente
+            checkBox.BringToFront();
         }
     }
 }
